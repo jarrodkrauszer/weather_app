e83ff4186f858bc1f4bfbeb8cf4103578693c645
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-20 18:49:13 -0400

    worked on coding the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,94 @@
 var searchBtns = document.querySelector(".locations");
 var cityInput = document.querySelector("#city-name");
+var weatherSection = document.querySelector('#weather-forecast');
+var fiveDay = document.querySelector('.five-day');
 
-const baseUrl = 'https://api.openweathermap.org';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
 const apiKey = '6c08e5624956619462f4e304a0abb23c';
+
+function displaySavedCities() {
+  var citiesList = document.querySelector('.cities');
+  var citiesFromStorage = getItemsFromStorage();
+
+  citiesList.innerHTML = '';
+
+  citiesFromStorage.forEach(function(city) {
+    var cityEl = document.createElement('button');
+    var cityName = document.createTextNode(city);
+
+    cityEl.className = 'city-button';
+
+    cityEl.appendChild(cityName);
+    citiesList.appendChild(cityEl);
+  });
+}
 
 function addItemToStorage(item) {
   const itemsFromStorage = getItemsFromStorage();
-  
-  itemsFromStorage.push(item);
 
-  localStorage.setItem('cities', JSON.stringify(itemsFromStorage));
+  if(!itemsFromStorage.includes(item)) {
+    itemsFromStorage.push(item);
+
+    localStorage.setItem('cities', JSON.stringify(itemsFromStorage));
+  }
+  
 }
 
 function getItemsFromStorage() {
   return JSON.parse(localStorage.getItem('cities')) || [];
 }
 
-function getWeather(lat, lon) {
+function getForecast(lat, lon) {
+  // fiveDay.innerHTML = '';
 
+  fetch(`${baseUrl}/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
+  .then(function (response) {
+    return response.json();
+  }).then(function(data) {
+    data.list.forEach(function(day) {
+      if(day.dt_txt.includes('12:00:00')) {
+        console.log(day);
+        // fiveDay.innerHTML += `<div class="day">
+        // <h4>${new Date(day.dt * 1000).toLocaleDateString()}</h4>
+        // <img
+        //   src="https://openweathermap.org/img/wn/${day.weather[0].icon}.png"
+        //   alt="Weather Icon"
+        // />
+        // <p>Temp: ${day.main.temp}<span> &#8457;</span></p>
+        // <p>Wind: ${day.wind.speed}<span> MPH</span></p>
+        // <p>Humidity: ${day.main.humidity}<span>%</span></p>
+        // </div>`;  
+      }
+    })
+  });
 }
 
-function requestCoords(city) {
+function getWeather(city) {
   
-  fetch(`${baseUrl}/geo/1.0/direct?q=${city}&mode=json&limit=1&appid=${apiKey}`)
+  fetch(`${baseUrl}/weather?q=${city}&units=imperial&appid=${apiKey}`)
     .then(function (response) {
-      var dataPromise = response.json();
-      dataPromise.then(function(data) {
-        getWeather(data[0].lat, data[0].lon);
+      return response.json();
+    }).then(function(data) {
 
-        var newCity = {
-          name: city,
-          lat: data[0].lat,
-          lon: data[0].lon
-        };
+      getForecast(data.coord.lat, data.coord.lon);
+      weatherSection.classList.remove('hidden');
 
-        addItemToStorage(newCity);
-      });
+      var cityNameEl = document.querySelector('#city-date');
+      var todaysIconEl = document.querySelector('#todays-icon');
+      var todaysTempEl = document.querySelector('#todays-temp');
+      var todaysWindEl = document.querySelector('#todays-wind');
+      var todaysHumidityEl = document.querySelector('#todays-humidity');
+
+      var timeFormatted = new Date(data.dt * 1000).toLocaleDateString();
+
+      cityNameEl.innerText = `${data.name}  (${timeFormatted})`
+      todaysIconEl.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+      todaysTempEl.innerText = data.main.temp;
+      todaysWindEl.innerText = data.wind.speed;
+      todaysHumidityEl.innerText = data.main.humidity;
+
+      addItemToStorage(city);
+      displaySavedCities();
     });
 }
 
@@ -45,11 +98,11 @@ function buttonClicked(e) {
   if (e.target.tagName === 'BUTTON') {
     switch (className) {
       case 'search-button':
-        requestCoords(cityInput.value);
+        getWeather(cityInput.value);
         break;
 
       case 'city-button':
-        getCoords('New York')
+        getWeather(e.target.textContent)
         break;
     }
   }
@@ -58,3 +111,4 @@ function buttonClicked(e) {
 }
 
 searchBtns.addEventListener('click', buttonClicked);
+window.addEventListener('load', displaySavedCities);
